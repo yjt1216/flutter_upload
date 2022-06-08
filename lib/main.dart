5f42c8ef0427ae4f6600d0e15617c6e4143456ec
尹江涛 <1904558423@qq.com>
2022-06-08 16:09:01 +0800

    upload page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,115 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_upload/pages/upload_file_page.dart';
+import 'package:flutter_upload/pages/widget/responses_screen.dart';
+import 'package:flutter_uploader/flutter_uploader.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+const String title = 'FileUpload Sample app';
+final Uri uploadURL = Uri.parse(
+  'https://us-central1-flutteruploadertest.cloudfunctions.net/upload',
+);
+
+FlutterUploader _uploader = FlutterUploader();
+
+void backgroundHandler() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Notice these instances belong to a forked isolate.
+  var uploader = FlutterUploader();
+
+  var notifications = FlutterLocalNotificationsPlugin();
+
+  // Only show notifications for unprocessed uploads.
+  SharedPreferences.getInstance().then((preferences) {
+    var processed = preferences.getStringList('processed') ?? <String>[];
+
+    if (Platform.isAndroid) {
+      uploader.progress.listen((progress) {
+        if (processed.contains(progress.taskId)) {
+          return;
+        }
+
+        notifications.show(
+          progress.taskId.hashCode,
+          'FlutterUploader Example',
+          'Upload in Progress',
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              'FlutterUploader.Example',
+              'FlutterUploader',
+              channelDescription:
+              'Installed when you activate the Flutter Uploader Example',
+              progress: progress.progress ?? 0,
+              icon: 'ic_upload',
+              enableVibration: false,
+              importance: Importance.low,
+              showProgress: true,
+              onlyAlertOnce: true,
+              maxProgress: 100,
+              channelShowBadge: false,
+            ),
+            iOS: const IOSNotificationDetails(),
+          ),
+        );
+      });
+    }
+
+    uploader.result.listen((result) {
+      if (processed.contains(result.taskId)) {
+        return;
+      }
+
+      processed.add(result.taskId);
+      preferences.setStringList('processed', processed);
+
+      notifications.cancel(result.taskId.hashCode);
+
+      final successful = result.status == UploadTaskStatus.complete;
+
+      var title = 'Upload Complete';
+      if (result.status == UploadTaskStatus.failed) {
+        title = 'Upload Failed';
+      } else if (result.status == UploadTaskStatus.canceled) {
+        title = 'Upload Canceled';
+      }
+
+      notifications
+          .show(
+        result.taskId.hashCode,
+        'FlutterUploader Example',
+        title,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            'FlutterUploader.Example',
+            'FlutterUploader',
+            channelDescription:
+            'Installed when you activate the Flutter Uploader Example',
+            icon: 'ic_upload',
+            enableVibration: !successful,
+            importance: result.status == UploadTaskStatus.failed
+                ? Importance.high
+                : Importance.min,
+          ),
+          iOS: const IOSNotificationDetails(
+            presentAlert: true,
+          ),
+        ),
+      )
+          .catchError((e, stack) {
+        print('error while showing notification: $e, $stack');
+      });
+    });
+  });
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -32,55 +137,100 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  int _currentIndex = 0;
+
+  bool allowCellular = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _uploader.setBackgroundHandler(backgroundHandler);
+
+    var flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    var initializationSettingsAndroid =
+    const AndroidInitializationSettings('ic_upload');
+    var initializationSettingsIOS = IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: true,
+      onDidReceiveLocalNotification:
+          (int id, String? title, String? body, String? payload) async {},
+    );
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: (payload) async {},
+    );
+
+    SharedPreferences.getInstance()
+        .then((sp) => sp.getBool('allowCellular') ?? true)
+        .then((result) {
+      if (mounted) {
+        setState(() {
+          allowCellular = result;
+        });
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: (){
-                  Get.to(() => const UploadFilePage());
-                },
-                child: const Text('上传'),
-              ),
-              const SizedBox(height: 15.0,),
-              ElevatedButton(
-                onPressed: (){
-                  Get.to(() => const UploadFilePage());
-                },
-                child: const Text('上传'),
-              ),
-            ],
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(allowCellular
+                  ? Icons.signal_cellular_connected_no_internet_4_bar
+                  : Icons.wifi_outlined),
+              onPressed: () async {
+                final sp = await SharedPreferences.getInstance();
+                await sp.setBool('allowCellular', !allowCellular);
+                if (mounted) {
+                  setState(() {
+                    allowCellular = !allowCellular;
+                  });
+                }
+              },
+            ),
+          ],
+        ),
+        body: _currentIndex == 0
+            ? UploadFilePage(
+          uploader: _uploader,
+          uploadURL: uploadURL,
+          onUploadStarted: () {
+            setState(() => _currentIndex = 1);
+          },
+        )
+            : ResponsesScreen(
+          uploader: _uploader,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.cloud_upload),
+              label: 'Upload',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.receipt),
+              label: 'Responses',
+            ),
+          ],
+          onTap: (newIndex) {
+            setState(() => _currentIndex = newIndex);
+          },
+          currentIndex: _currentIndex,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
 }
--- lib/pages/upload_file_page.dart
+++ lib/pages/upload_file_page.dart
@@ -1,28 +1,244 @@
 
 
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_uploader/flutter_uploader.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'model/server_behavior.dart';
 
 class UploadFilePage extends StatefulWidget {
-  const UploadFilePage({Key? key}) : super(key: key);
+
+  final FlutterUploader uploader;
+  final Uri uploadURL;
+  final VoidCallback onUploadStarted;
+
+
+  const UploadFilePage({Key? key,
+    required this.uploader,
+    required this.uploadURL,
+    required this.onUploadStarted}) : super(key: key);
 
   @override
   _UploadFilePageState createState() => _UploadFilePageState();
 }
+
 class _UploadFilePageState extends State<UploadFilePage> {
+  ImagePicker imagePicker = ImagePicker();
+
+  ServerBehavior _serverBehavior = ServerBehavior.defaultOk200;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (Platform.isAndroid) {
+      imagePicker.retrieveLostData().then((lostData) {
+        if (lostData.isEmpty) {
+          return;
+        }
+
+        if (lostData.type == RetrieveType.image) {
+          _handleFileUpload([lostData.file!.path]);
+        }
+        if (lostData.type == RetrieveType.video) {
+          _handleFileUpload([lostData.file!.path]);
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: const Text('上传任务'),
+        title: const Text('Flutter Uploader'),
       ),
-      body: Container(
-        color: const Color.fromRGBO(255, 255, 255, 1),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Configure test Server Behavior',
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                DropdownButton<ServerBehavior>(
+                  items: ServerBehavior.all.map((e) {
+                    return DropdownMenuItem(
+                      value: e,
+                      child: Text(e.title),
+                    );
+                  }).toList(),
+                  onChanged: (newBehavior) {
+                    if (newBehavior != null) {
+                      setState(() => _serverBehavior = newBehavior);
+                    }
+                  },
+                  value: _serverBehavior,
+                ),
+                const Divider(),
+                Text(
+                  'multipart/form-data uploads',
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                Wrap(
+                  alignment: WrapAlignment.center,
+                  spacing: 10,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () => getImage(binary: false),
+                      child: const Text('upload image'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => getVideo(binary: false),
+                      child: const Text('upload video'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => getMultiple(binary: false),
+                      child: const Text('upload multi'),
+                    ),
+                  ],
+                ),
+                const Divider(height: 40),
+                Text(
+                  'binary uploads',
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                const Text('this will upload selected files as binary'),
+                Wrap(
+                  alignment: WrapAlignment.center,
+                  spacing: 10,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () => getImage(binary: true),
+                      child: const Text('upload image'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => getVideo(binary: true),
+                      child: const Text('upload video'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => getMultiple(binary: true),
+                      child: const Text('upload multi'),
+                    ),
+                  ],
+                ),
+                const Divider(height: 40),
+                const Text('Cancellation'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () => widget.uploader.cancelAll(),
+                      child: const Text('Cancel All'),
+                    ),
+                    Container(width: 20.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        widget.uploader.clearUploads();
+                      },
+                      child: const Text('Clear Uploads'),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 
+  Future getImage({required bool binary}) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('binary', binary);
 
+    var image = await imagePicker.pickImage(source: ImageSource.gallery);
 
+    if (image != null) {
+      _handleFileUpload([image.path]);
+    }
+  }
 
+  Future getVideo({required bool binary}) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('binary', binary);
+
+    var video = await imagePicker.pickVideo(source: ImageSource.gallery);
+
+    if (video != null) {
+      _handleFileUpload([video.path]);
+    }
+  }
+
+  Future getMultiple({required bool binary}) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('binary', binary);
+
+    final files = await FilePicker.platform.pickFiles(
+      allowCompression: false,
+      allowMultiple: true,
+    );
+    if (files != null && files.count > 0) {
+      if (binary) {
+        for (var file in files.files) {
+          _handleFileUpload([file.path]);
+        }
+      } else {
+        _handleFileUpload(files.paths);
+      }
+    }
+  }
+
+  void _handleFileUpload(List<String?> paths) async {
+    final prefs = await SharedPreferences.getInstance();
+    final binary = prefs.getBool('binary') ?? false;
+    final allowCellular = prefs.getBool('allowCellular') ?? true;
+
+    await widget.uploader.enqueue(_buildUpload(
+      binary,
+      paths.whereType<String>().toList(),
+      allowCellular,
+    ));
+
+    widget.onUploadStarted();
+  }
+
+  Upload _buildUpload(bool binary, List<String> paths,
+      [bool allowCellular = true]) {
+    const tag = 'upload';
+
+    var url = binary
+        ? widget.uploadURL.replace(path: widget.uploadURL.path + 'Binary')
+        : widget.uploadURL;
+
+    url = url.replace(queryParameters: {
+      'simulate': _serverBehavior.name,
+    });
+
+    if (binary) {
+      return RawUpload(
+        url: url.toString(),
+        path: paths.first,
+        method: UploadMethod.POST,
+        tag: tag,
+        allowCellular: allowCellular,
+      );
+    } else {
+      return MultipartFormDataUpload(
+        url: url.toString(),
+        data: {'name': 'john'},
+        files: paths.map((e) => FileItem(path: e, field: 'file')).toList(),
+        method: UploadMethod.POST,
+        tag: tag,
+        allowCellular: allowCellular,
+      );
+    }
+  }
 }
